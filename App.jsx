32c3ecadfc94d9a9a70ef856e6491ea4b949135c
Aniket Kumar Singh <singhanita06798@gmail.com>
2026-06-18 22:14:49 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,44 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Student from './Student'
+import StudentForm from "./components/StudentForm";
+import "./App.css";
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <h2>React Counter Application</h2>
-      const increment
-      {/*
-      <h2>Student Information</h2>
-      <Student name="Ram" course="B.Tech" marks="90"/><br/>
-      <Student name="Raj" course="M.Tech" marks="96"/><br/>
-      <Student name="Rohan" course="MCA" marks="98"/>
-      */}
-
-      {/*
-      <video controls>
-        <source src="" type="video/mp4/"></source>
-      </video>
-      <audio controls>
-        <source src="" type="audio/mp3/"></source>
-      </audio>
-      <iframe src="https://www.youtube.com/embed/vHt_tCREXbY" allowFullScreen></iframe>
-      <iframe src="https://www.youtube.com/embed/LW_Yj80rlv4" allowFullScreen></iframe>
-    <img src="https://images.pexels.com/photos/20447506/pexels-photo-20447506.jpeg" width="1000" height="500"></img><br></br>
-    <button>Click Me</button>
-    <h1>My Web Page of  ABES</h1>
-    <h2>Hello Everyone</h2>
-    <h3>hello</h3>
-    <p>Hello Rohit Sir</p>
-    <h1>vgjgjgjhjjhjjb</h1>
-    <p>rrrrrrrrrrrrrrr</p>
-    */}
-    <Student/>
-    </div> 
-  )
+    <div className="container">
+      <h1>Student Registration System</h1>
+      <StudentForm />
+    </div>
+  );
 }
 
-export default App
+export default App;
